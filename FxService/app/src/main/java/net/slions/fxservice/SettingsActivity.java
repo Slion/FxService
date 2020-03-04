1beda462da07745a420bb034bdcdc90085699aea
Stéphane Lenclud <github@lenclud.com>
2020-03-04 16:27:09 +0100

    Simplified implementation of function checking if accessibility service is enabled.

--- FxService/app/src/main/java/net/slions/fxservice/SettingsActivity.java
+++ FxService/app/src/main/java/net/slions/fxservice/SettingsActivity.java
@@ -44,9 +44,12 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Make sure the back button closes the application
-        switch (item.getItemId()) {
+        // See: https://stackoverflow.com/questions/14545139/android-back-button-in-the-title-bar
+        switch (item.getItemId())
+        {
             case android.R.id.home:
                 finish();
                 return true;
@@ -87,61 +90,17 @@ public class SettingsActivity extends AppCompatActivity {
         super.onPostResume();
 
         SwitchPreferenceCompat pref = (SwitchPreferenceCompat) iSettingsFragment.findPreference(getResources().getString(R.string.pref_key_accessibility_service));
-
-        //boolean isdocumentModeEnabled = !mDocumentModeManager.isOptedOutOfDocumentMode();
-        pref.setChecked(isAccessibilitySettingsOn(this));
+        pref.setChecked(isAccessServiceEnabled(this,FxService.class));
 
     }
 
-    // To check if service is enabled
-    private boolean isAccessibilitySettingsOn(Context mContext)
+    // To check if an accessibility service is enabled
+    // See: https://stackoverflow.com/questions/18094982/detect-if-my-accessibility-service-is-enabled/18095283
+    public boolean isAccessServiceEnabled(Context context, Class accessibilityServiceClass)
     {
-        int accessibilityEnabled = 0;
-        final String service = getPackageName() + "/" + FxService.class.getCanonicalName();
-        try
-        {
-            accessibilityEnabled = Settings.Secure.getInt(
-                    mContext.getApplicationContext().getContentResolver(),
-                    android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
-            Log.v(TAG, "accessibilityEnabled = " + accessibilityEnabled);
-        }
-        catch (Settings.SettingNotFoundException e)
-        {
-            Log.e(TAG, "Error finding setting, default accessibility to not found: "
-                    + e.getMessage());
-        }
-        TextUtils.SimpleStringSplitter mStringColonSplitter = new TextUtils.SimpleStringSplitter(':');
-
-        if (accessibilityEnabled == 1)
-        {
-            Log.v(TAG, "***ACCESSIBILITY IS ENABLED*** -----------------");
-            String settingValue = Settings.Secure.getString(
-                    mContext.getApplicationContext().getContentResolver(),
-                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
-            if (settingValue != null)
-            {
-                mStringColonSplitter.setString(settingValue);
-                while (mStringColonSplitter.hasNext())
-                {
-                    String accessibilityService = mStringColonSplitter.next();
-
-                    Log.v(TAG, "-------------- > accessibilityService :: " + accessibilityService + " " + service);
-                    if (accessibilityService.equalsIgnoreCase(service))
-                    {
-                        Log.v(TAG, "We've found the correct setting - accessibility is switched on!");
-                        return true;
-                    }
-                }
-            }
-        }
-        else
-        {
-            Log.v(TAG, "***ACCESSIBILITY IS DISABLED***");
-        }
-
-        return false;
+        String prefString = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+        return prefString!= null && prefString.contains(context.getPackageName() + "/" + accessibilityServiceClass.getName());
     }
-
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
